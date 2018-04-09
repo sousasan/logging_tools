93e2817c0852e92e5ec5c212735ea9ea4cbf0c83
Sandra Sousa <sandra@quandl.com>
2018-04-09 13:47:53 -0400

    make package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='logging_tools',
+      version='0.1',
+      description='Tools for logging',
+      url='http://github.com/sousasan/logging_tools',
+      author='Data Team',
+      author_email='data@quandl.com',
+      packages=['logging'],
+      zip_safe=False)
