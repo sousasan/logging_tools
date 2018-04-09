d2cc28942d709836f21143e0b49ccbc7e24d40f9
Sandra Sousa <sandra@quandl.com>
2018-04-09 15:19:16 -0400

    increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='logging_tools',
-      version='0.1',
+      version='0.2',
       description='Tools for logging',
       url='http://github.com/sousasan/logging_tools',
       author='Data Team',
